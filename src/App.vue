4f10a42a19b07ebe403cd8258d125a84f1c8aa9f
doej96 <doej96@naver.com>
2021-02-18 11:28:50 +0900

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <MainTitle />
+    <SearchForm />
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainTitle from './components/MainTitle.vue'
+import SearchForm from './components/SearchForm.vue'
 export default {
   name: 'App',
+  data() {
+    return {
+
+    }
+  },
   components: {
-    HelloWorld
+    MainTitle
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  
 </style>
---
+++ src/components/SearchForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <form>
+      <SearchInput />
+    </form>
+  </div>
+</template>
+<script>
+import SearchInput from './SearchInput.vue'
+export default {
+  name: 'SearchForm',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    SearchInput
+  }
+}
+</script>
+<style scoped>
+  
+</style>
